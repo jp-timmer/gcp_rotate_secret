baf438ba34563b9e60b23afc14c38b3a303905c1
Jelmer Timmer <jelmer.timmer@ingka.ikea.com>
2022-04-08 11:44:38 +0200

    Rewrite code to work as a cloud function

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ import secrets
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # [START secretmanager_add_secret_version]
-def add_secret_version(project_id, secret_id, payload):
+def add_secret_version(event, context):
     """
-    Update the metadata about an existing secret.
+    Add new randomly generated secret version to the secret.
     """
 
     from google.cloud import secretmanager
 
     client = secretmanager.SecretManagerServiceClient()
-
+    project_id = os.getenv("GCP_PROJECT_ID")
+    secret_id = os.getenv("GCP_SECRET_ID")
+    
     parent = client.secret_path(project_id, secret_id)
-
+    payload = generate_random_string(30)
     payload = payload.encode("UTF-8")
-
+    print('Payload: {}'.format(payload))
     crc32c = google_crc32c.Checksum()
     crc32c.update(payload)
 
@@ -32,13 +36,10 @@ def add_secret_version(project_id, secret_id, payload):
     print("Added secret version: {}".format(response.name))
     # [END secretmanager_add_secret_version]
 
-    return response
-
-if __name__ == "__main__":
-    load_dotenv()
-    project_id = os.getenv("GCP_PROJECT_ID")
-    secret_id = os.getenv("GCP_SECRET_ID")
+# [START secretmanager_generate_random_string]
+def generate_random_string(length):
     alphabet = string.ascii_letters + string.digits
-    passwords = ''.join(secrets.choice(alphabet) for i in range(30))
-
-    add_secret_version(project_id, secret_id, passwords)
+    passwords = ''.join(secrets.choice(alphabet) for i in range(length))
+    print ("Generated password of ", length, "is:", passwords)
+    return passwords
+    # [END secretmanager_generate_random_string]
